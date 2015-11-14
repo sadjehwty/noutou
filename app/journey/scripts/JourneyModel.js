a178f880e215b7a461e24b107f810b88b0262e9d
Davide Rizzi <sadjehwty@gmail.com>
2015-11-14 12:11:16 +0100

    use custom function

--- app/journey/scripts/JourneyModel.js
+++ app/journey/scripts/JourneyModel.js
@@ -1,3 +1,6 @@
 angular
   .module('journey')
   .constant('Journey', supersonic.data.model('journey'));
+supersonic.data.model('journey').prototype.is_close=function(){
+  return this.closed!='';
+};
