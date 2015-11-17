df14183cf0fbb81f1713d315876f41b04c0f34ed
Davide Rizzi <sadjehwty@gmail.com>
2015-11-17 21:20:01 +0100

    start merging show journey with index cost

--- app/journey/scripts/JourneyModel.js
+++ app/journey/scripts/JourneyModel.js
@@ -1,12 +1,11 @@
 angular
   .module('journey')
-  .constant('Journey', supersonic.data.model('journey'))
-  .run(function(){
-    var Journey=supersonic.data.model('journey');
-    Journey.prototype.costs=function(){
-      var Cost=supersonic.data.model('cost');
-      return Cost.all({id_journey: this.id});
-    };
-    Journey.prototype.is_close=function(){
-      return this.closed!='';
-    });
+  .constant('Journey', supersonic.data.model('journey'));
+var Journey=supersonic.data.model('journey');
+Journey.prototype.costs=function(){
+  var Cost=supersonic.data.model('cost');
+  return Cost.all({id_journey: this.id});
+};
+Journey.prototype.is_close=function(){
+  return this.closed!='';
+};
