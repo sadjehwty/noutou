a974f9c07a9152818244f28a0df0a56d98e8d693
Davide Rizzi <davide.rizzi@nethical.net>
2015-11-18 09:00:42 +0100

    fix show journey & new cost

--- app/cost/scripts/NewController.js
+++ app/cost/scripts/NewController.js
@@ -2,12 +2,19 @@ angular
   .module('cost')
   .controller("NewController", function ($scope, Cost, supersonic) {
     $scope.cost = {};
-
+    $scope.showSpinner = true;
+    
+    supersonic.ui.views.current.params.onValue( function (values) {
+      $scope.cost = {id_journey: values.id};
+      $scope.showSpinner = false;
+    });
+    
     $scope.submitForm = function () {
       $scope.showSpinner = true;
       newcost = new Cost($scope.cost);
       newcost.save().then( function () {
         supersonic.ui.modal.hide();
+        _refreshViewData();
       });
     };
 
