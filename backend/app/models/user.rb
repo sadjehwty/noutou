3c38d361312eae7257c870c742fff956f8e12fcc
sadjehwty <sadjehwty@gmail.com>
2017-12-04 14:34:28 +0100

    corrette relazioni di amicizia

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, presence: true
-  has_many :users, through: :friends
+  has_many :friendships, :foreign_key => "user_id",:class_name=>"FirendShip"
+  has_many :friends, through: :friendships
   has_many :travels, through: :groups
   has_many :shares
 end
--- backend/db/migrate/20171201064031_create_friends.rb
+++ backend/db/migrate/20171201064031_create_friends.rb
@@ -1,8 +1,8 @@
 class CreateFriends < ActiveRecord::Migration[5.1]
   def change
     create_table :friends, id: false  do |t|
-      t.belongs_to :from_id, index: true
-      t.belongs_to :to_id, index: true
+      t.belongs_to :user_id, index: true
+      t.belongs_to :friend_id, index: true
     end
   end
 end
