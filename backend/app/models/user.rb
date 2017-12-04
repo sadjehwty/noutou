7b8818334ebbbc4d977bd6eb162ce409230a86cd
Davide Rizzi <sadjehwty@gmail.com>
2017-12-04 21:38:50 +0100

    sostituito friend

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, presence: true
+  has_many :friendships, :foreign_key => "user_id", :class_name => "Friendship"
+  has_many :friends, :through => :friendships
 end
--- backend/db/migrate/20171201064031_create_friends.rb
+++
@@ -1,8 +0,0 @@
-class CreateFriends < ActiveRecord::Migration[5.1]
-  def change
-    create_table :friends, id: false  do |t|
-      t.belongs_to :user_id, index: true
-      t.belongs_to :friend_id, index: true
-    end
-  end
-end
