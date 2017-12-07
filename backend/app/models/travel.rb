ab8fc7e29cfb681afe6418ceec34e981066f310c
sadjehwty <sadjehwty@gmail.com>
2017-12-07 13:53:30 +0100

    migliorata validazione travel

--- backend/app/models/travel.rb
+++ backend/app/models/travel.rb
@@ -3,9 +3,9 @@ class Travel < ApplicationRecord
   belongs_to :user
   has_one :group, dependent: :destroy, autosave: true
   has_many :users, through: :group
-  after_create do |travel|
-	  travel.create_group
-	  travel.group.users << travel.user
+  before_validation do |travel|
+    travel.build_group if travel.group.nil?
+    travel.group.users << travel.user
   end
-  validates :user, inclusion: { in: users }
+  validates :user, inclusion: { in: :users }
 end
