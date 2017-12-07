6e50fb6d26d03a1bd9d33ce5c5231667e49bead0
sadjehwty <sadjehwty@gmail.com>
2017-12-07 08:05:45 +0100

    creazione jwt e autenticazione

---
+++ backend/app/controllers/session_controller.rb
@@ -0,0 +1,12 @@
+class SessionController < ApplicationController
+  skip_before_action :authenticate_request
+
+  def jwt
+    command = AuthenticateUser.call(params[:email], params[:password])
+    if command.success?
+      render json: { auth_token: command.result }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'session/jwt'
+
   resources :shares
   resources :costs
   resources :travels
