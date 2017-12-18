3f57cf0a3ee65fb222f837ae249f8eb1236b9e8a
Davide Rizzi <sadjehwty@gmail.com>
2017-12-18 21:38:18 +0100

    corrette validazioni

--- backend/app/models/share.rb
+++ backend/app/models/share.rb
@@ -2,5 +2,7 @@ class Share < ApplicationRecord
   validates :value, numericality: true
   belongs_to :cost
   belongs_to :user
-  validates :user, inclusion: { in: cost.travel.users }
+  validate do |share|
+    errors[:user] << 'Share from User not in Group' unless share.cost.travel.group.users.include? share.user
+  end
 end
