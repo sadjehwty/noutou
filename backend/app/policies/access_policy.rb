da9dcc6d9232587172e0796a57fb38ab59b8615c
Davide Rizzi <sadjehwty@gmail.com>
2017-12-18 21:35:53 +0100

    copia incolla errato in policy

--- backend/app/policies/access_policy.rb
+++ backend/app/policies/access_policy.rb
@@ -10,51 +10,51 @@ class AccessPolicy
     # The base role with no additional conditions.
     # Applies to every user.
     role :guest do
-     # User
-     can :read, User
-     can :seach, User
-     can :create, User
-     can :merge, User do |obj,usr|
-       !obj.merge_code.nil?
-     end
-     can :destroy, User do |obj,usr|
-       obj.friends.contains?(usr) && obj.merge_code.nil?
-     end
-     can :update, User do |obj, usr|
-       obj==usr || (obj.friends.contains?(usr) && obj.merge_code.nil?)
-     end
-     # Travel
-     can :read, Travel do |obj,usr|
-       obj.user==usr || obj.group.users.contains?(usr)
-     end
-     can :create, Travel
-     can :update, Travel do |obj,usr|
-       obj.user==usr
-     end
-     can :destroy, Travel do |obj,usr|
-       obj.user==usr && obj.group.users.count == 1
-     end
-     # Group
-     can :read, Group do |obj,usr|
-       obj.users.contains?(usr)
-     end
-     can :update, Group do |obj,usr|
-       obj.travel.user==usr
-     end
-     # Costs
-     can :read, Cost do |obj,usr|
-       can? :read, obj.travel
-     end
-     can [:update,:destroy,:create], Cost do |obj,usr|
-       can? :update, obj.travel
-     end
-     # Shares
-     can :read, Share do |obj,usr|
-       can? :read, obj.cost
-     end
-     can [:update,:destroy,:create], Share do |obj,usr|
-       can? :update, obj.cost
-     end
+      # User
+      can :read, User
+      can :seach, User
+      can :create, User
+      can :merge, User do |obj,usr|
+        !obj.merge_code.nil?
+      end
+      can :destroy, User do |obj,usr|
+        obj.friends.include?(usr) && obj.merge_code.nil?
+      end
+      can :update, User do |obj, usr|
+        obj==usr || (obj.friends.include?(usr) && obj.merge_code.nil?)
+      end
+      # Travel
+      can :read, Travel do |obj,usr|
+        obj.user==usr || obj.group.users.include?(usr)
+      end
+      can :create, Travel
+      can :update, Travel do |obj,usr|
+        obj.user==usr
+      end
+      can :destroy, Travel do |obj,usr|
+        obj.user==usr && obj.group.users.count == 1
+      end
+      # Group
+      can :read, Group do |obj,usr|
+        obj.users.include?(usr)
+      end
+      can :update, Group do |obj,usr|
+        obj.travel.user==usr
+      end
+      # Costs
+      can :read, Cost do |obj,usr|
+        can? :read, obj.travel
+      end
+      can [:update,:destroy,:create], Cost do |obj,usr|
+        can? :update, obj.travel
+      end
+      # Shares
+      can :read, Share do |obj,usr|
+        can? :read, obj.cost
+      end
+      can [:update,:destroy,:create], Share do |obj,usr|
+        can? :update, obj.cost
+      end
     end
   end
 end
