62a9f61c443dc0f94eb5c1ca844e2e3c054488c9
sadjehwty <sadjehwty@gmail.com>
2017-12-18 13:22:55 +0100

    aggiunta associazione user2travel tramite gruppo

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
   has_many :friendships, foreign_key: "user_id", class_name: "Friendship", dependent: :destroy, autosave: true
   has_many :friends, through: :friendships
-  has_many :travels, dependent: :destroy
+  has_many :journeys, source: :travels, class_name: 'Travel', dependent: :destroy
   has_and_belongs_to_many :groups
   has_many :shares, dependent: :destroy
+  has_many :travels, through: :groups
   after_create do |user|
 	  user.friends << user
   end
