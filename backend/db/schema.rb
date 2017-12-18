76325a4e22468900fc9dcf79e28bc7ef2efaa752
Davide Rizzi <sadjehwty@gmail.com>
2017-12-18 21:33:50 +0100

    aggiornato db

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211065620) do
+ActiveRecord::Schema.define(version: 20171218115339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20171211065620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "merge_code"
   end
 
   add_foreign_key "costs", "travels"
