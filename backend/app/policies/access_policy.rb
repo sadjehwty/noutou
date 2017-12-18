40721865043210a14250ef0195b4f7fbce67b1a8
Davide Rizzi <sadjehwty@gmail.com>
2017-12-18 16:03:08 +0100

    Pulizia codice

--- backend/app/policies/access_policy.rb
+++ backend/app/policies/access_policy.rb
@@ -7,28 +7,6 @@ class AccessPolicy
     #
     # https://github.com/chaps-io/access-granted/blob/master/README.md
     #
-    # Roles inherit from less important roles, so:
-    # - :admin has permissions defined in :member, :guest and himself
-    # - :member has permissions from :guest and himself
-    # - :guest has only its own permissions since it's the first role.
-    #
-    # The most important role should be at the top.
-    # In this case an administrator.
-    #
-    # role :admin, proc { |user| user.admin? } do
-    #   can :destroy, User
-    # end
-
-    # More privileged role, applies to registered users.
-    #
-    # role :member, proc { |user| user.registered? } do
-    #   can :create, Post
-    #   can :create, Comment
-    #   can [:update, :destroy], Post do |post, user|
-    #     post.author == user
-    #   end
-    # end
-
     # The base role with no additional conditions.
     # Applies to every user.
     role :guest do
@@ -42,7 +20,7 @@ class AccessPolicy
        obj.friends.contains?(usr) && obj.merge_code.nil?
      end
      can :update, User do |obj, usr|
-       obj.id==usr.id || (obj.friends.contains?(usr) && obj.merge_code.nil?)
+       obj==usr || (obj.friends.contains?(usr) && obj.merge_code.nil?)
      end
     end
   end
