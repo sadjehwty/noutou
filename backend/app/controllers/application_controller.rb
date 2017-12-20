d5dcf1a928db34bcc81d9f73e331f18ac3b57c66
sadjehwty <sadjehwty@gmail.com>
2017-12-20 13:55:13 +0100

    pulizia codice controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,15 +1,12 @@
 class ApplicationController < ActionController::API
-  include ActionController::Helpers
   before_action :authenticate_request
   rescue_from "AccessGranted::AccessDenied" do |exception|
     render json: { error: 'Forbidden' }, status: 403
   end
   
   attr_reader :current_user
-  helper_method :current_user
 
   private
-
   def authenticate_request
     @current_user = AuthenticateApiRequest.call(request.headers).result
     render json: { error: 'Not Authorized' }, status: 401 unless @current_user
