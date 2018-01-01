3b2791a9dbbe816cc611a67774b521d9f32913a7
Davide Rizzi <sadjehwty@gmail.com>
2018-01-01 11:53:20 +0100

    corretto test

--- backend/test/controllers/sessions_controller_test.rb
+++ backend/test/controllers/sessions_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should post jwt" do
-    get sessions_jwt_url
+  test "should create session" do
+    get users_url, as: :json
     assert_response :success
   end
 
