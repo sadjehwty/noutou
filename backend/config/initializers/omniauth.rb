41d0e969a2b92a1121b1691cc0c18aadd76dd89a
Davide Rizzi <sadjehwty@gmail.com>
2018-01-02 11:05:16 +0100

    aggiunti altri provider

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.facebook_app, Rails.application.secrets.facebook_secret,
     scope: 'email,public_profile', info_fields: 'email,first_name,last_name,id'
+  provider :linkedin, Rails.application.secrets.linkedin_app, Rails.application.secrets.linkedin_secret,
+    scope: 'r_fullprofile r_emailaddress', fields: ['id', 'email-address', 'first-name', 'last-name']
+  provider :google_oauth2, Rails.application.secrets.google_app, Rails.application.secrets.google_secret,
+    scope: 'email,profile'
 end 
