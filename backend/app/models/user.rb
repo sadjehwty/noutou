08692560356f3f8dd248a1b1e842b2d0410c90e0
Davide Rizzi <sadjehwty@gmail.com>
2018-01-05 16:02:57 +0100

    corretta validazione

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :name,  presence: true
+  validates :surname,  presence: true
   has_many :friendships, foreign_key: "user_id", class_name: "Friendship", dependent: :destroy, autosave: true
   has_many :friends, through: :friendships
   has_many :journeys, source: :travels, class_name: 'Travel', dependent: :destroy
