82b6a24796c7f57543cfb3132480396be69755f2
Davide Rizzi <sadjehwty@gmail.com>
2018-01-05 15:59:04 +0100

    sostituita strategia

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,8 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.facebook_app, Rails.application.secrets.facebook_secret,
     scope: 'email,public_profile', info_fields: 'email,first_name,last_name,id'
-  provider :linkedin, Rails.application.secrets.linkedin_app, Rails.application.secrets.linkedin_secret,
-    scope: 'r_fullprofile r_emailaddress', fields: ['id', 'email-address', 'first-name', 'last-name']
+  provider :microsoft_live, Rails.application.secrets.windows_app, Rails.application.secrets.windows_secret
   provider :google_oauth2, Rails.application.secrets.google_app, Rails.application.secrets.google_secret,
     scope: 'email,profile'
 end 
