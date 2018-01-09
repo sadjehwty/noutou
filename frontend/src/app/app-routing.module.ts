fec26fd515967d678962d0b25349aeef0bb3ff66
sadjehwty <sadjehwty@gmail.com>
2018-01-09 09:47:47 +0100

    inizio routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { TravelsComponent }   from './travels/travels.component';
+import { UserDetailComponent }  from './user-detail/user-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'travels', component: TravelsComponent },
+  { path: 'users/:id', component: UserDetailComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
+
 export class AppRoutingModule { }
