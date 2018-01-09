73570be3ad6defe0a1f81c524f33e8ef46eb41e1
sadjehwty <sadjehwty@gmail.com>
2018-01-09 09:17:14 +0100

    componenti di indicizzazione

--- frontend/src/app/travels/travels.component.ts
+++ frontend/src/app/travels/travels.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Travel } from '../classes/travel';
+import { TravelService } from '../services/travel.service';
 
 @Component({
   selector: 'app-travels',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./travels.component.css']
 })
 export class TravelsComponent implements OnInit {
+  
+  travels: Travel[];
 
-  constructor() { }
+  constructor(private travelService: TravelService) { }
 
   ngOnInit() {
+    this.getTravels();
   }
-
+  
+  getTravels(): void {
+    this.travelService.getTravels().subscribe(travels => this.travels = travels);
+  }
+  
+  addTravel(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.travelService.addTravel({ name } as Travel).subscribe(travel => { this.travels.push(travel);});
+  }
+  
+  deleteTravel(travel: Travel): void {
+    this.travels = this.travels.filter(t => t !== travel);
+    this.travelService.deleteTravel(travel).subscribe();
+  }
 }
--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../classes/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
-
+  
+  users: User[];
+  
+  constructor(private userService: UserService) { }
+  
   ngOnInit() {
+    this.getUsers();
   }
-
+  
+  getUsers(): void {
+    this.userService.getUsers().subscribe(users => this.users = users);
+  }
+  
+  addUser(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.userService.addUser({ name } as User).subscribe(user => { this.users.push(user);});
+  }
+  
+  deleteUser(user: User): void {
+    this.users = this.users.filter(t => t !== user);
+    this.userService.deleteUser(user).subscribe();
+  }
 }
