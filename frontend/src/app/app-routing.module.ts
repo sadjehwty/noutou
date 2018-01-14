e2f916d0842bbe9481515c90d424ccfc5ccf32b0
Davide Rizzi <sadjehwty@gmail.com>
2018-01-14 10:30:06 +0100

    aggiunto path per show travel

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { TravelsComponent }   from './travels/travels.component';
 import { UserDetailComponent }  from './user-detail/user-detail.component';
+import { TravelDetailComponent }  from './travel-detail/travel-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
   { path: 'travels', component: TravelsComponent },
-  { path: 'users/:id', component: UserDetailComponent }
+  { path: 'users/:id', component: UserDetailComponent },
+  { path: 'travels/:id', component: TravelDetailComponent }
 ];
 
 @NgModule({
