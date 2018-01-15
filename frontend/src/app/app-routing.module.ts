21a79c2cb5297290e7eb62c0658f0cb4614e211d
sadjehwty <sadjehwty@gmail.com>
2018-01-15 14:57:47 +0100

    route per cost e share

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { UsersComponent } from './users/users.component';
 import { TravelsComponent }   from './travels/travels.component';
 import { UserDetailComponent }  from './user-detail/user-detail.component';
 import { TravelDetailComponent }  from './travel-detail/travel-detail.component';
+import { CostDetailComponent }  from './cost-detail/cost-detail.component';
+import { ShareDetailComponent }  from './share-detail/share-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
   { path: 'travels', component: TravelsComponent },
   { path: 'users/:id', component: UserDetailComponent },
-  { path: 'travels/:id', component: TravelDetailComponent }
+  { path: 'travels/:id', component: TravelDetailComponent },
+  { path: 'costs/:id', component: CostDetailComponent },
+  { path: 'shares/:id', component: ShareDetailComponent }
 ];
 
 @NgModule({
