900edc4c0723327f123b1f6d27e8946124e6b538
sadjehwty <sadjehwty@gmail.com>
2018-01-29 10:00:23 +0100

    gestione redirect

--- frontend/src/app/jwt-interceptor.ts
+++ frontend/src/app/jwt-interceptor.ts
@@ -4,11 +4,13 @@ import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/observable/throw'
 import 'rxjs/add/operator/catch';
 import { MessageService } from './services/message.service';
+import { Location } from '@angular/common';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService,
+              private location: Location) { }
   
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     
@@ -18,10 +20,15 @@ export class JwtInterceptor implements HttpInterceptor {
     //send the newly created request
     return next.handle(authReq)
     .catch((error, caught) => {
-      //intercept the respons error and displace it to the console
-      this.messageService.error(error.message);
-      //return the error to the method that called it
-      return Observable.throw(error);
+      if(error.status==401){
+        this.location.go('/login');
+        return Observable.throw('Redirect');
+      }else{
+        //intercept the respons error and displace it to the console
+        this.messageService.error(error.message);
+        //return the error to the method that called it
+        return Observable.throw(error);
+      }
     }) as any;
   }
 }
