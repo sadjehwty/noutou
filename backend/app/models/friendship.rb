2b478eae7b553093ae200513f8dc4718dbd396cb
sadjehwty <sadjehwty@gmail.com>
2018-02-01 10:50:22 +0100

    migliorato output friendship

--- backend/app/models/friendship.rb
+++ backend/app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
   belongs_to :user, :foreign_key => "user_id", :class_name => "User"
   belongs_to :friend, :foreign_key => "friend_id", :class_name => "User"
+  def as_json(options={})
+    super(include: :friend)
+  end
 end
