5d737b8214390e9fce7f37bd1a3b81003d506743
Davide Rizzi <sadjehwty@gmail.com>
2018-02-03 10:46:41 +0100

    diviso dominio da url base

--- frontend/src/app/app.globals.ts
+++ frontend/src/app/app.globals.ts
@@ -2,5 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppGlobals {
-  readonly baseAppUrl: string = 'http://jwt.macrobug.dev:3000/api';
+  readonly baseAppDomain: string = 'http://jwt.macrobug.dev:3000';
+  readonly baseAppUrl: string = this.baseAppDomain+'/api';
 }
