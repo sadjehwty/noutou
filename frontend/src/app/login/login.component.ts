c819ea23880beeedd1fea82f4fa797887491a3e0
sadjehwty <sadjehwty@gmail.com>
2018-02-12 14:12:52 +0100

    corrette chiamate a nuovo metodo

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -84,7 +84,7 @@ export class LoginComponent implements OnInit {
     });
   }
   private
-  objectToFormData(obj, form, namespace):FormData {
+  objectToFormData(obj: any, form?: FormData, namespace?: string):FormData {
     var fd = form || new FormData();
     var formKey;
     for(var property in obj) {
@@ -97,7 +97,7 @@ export class LoginComponent implements OnInit {
         // if the property is an object, but not a File,
         // use recursivity.
         if(typeof obj[property] === 'object' && !(obj[property] instanceof File)) {
-          objectToFormData(obj[property], fd, property);
+          this.objectToFormData(obj[property], fd, property);
         } else {
           // if it's a string or a File object
           fd.append(formKey, obj[property]);
